3c460f904679ae634b615767a927dc92c560b392
TonyYou998 <19520931@gm.uit.edu.com>
2021-03-22 21:05:36 +0700

    remove some folders

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
 import listMovieReducer from "../../containers/HomeTemplate/HomePage/modules/ListMovieReducers";
 import NewReducers from "../../containers/HomeTemplate/HomePage/modules/NewReducers";
-import btgamebaucuareducer from "./btgamebaucuareducer";
+
 import DetailMovieReducer from "../../containers/HomeTemplate/DetaiMovie/Modules/DetailMoviereduce";
 import listCinemaReducer from "../../containers/HomeTemplate/HomePage/modules/ListCinemaReducer";
 import listCinemaShowtimeReducer from "../../containers/HomeTemplate/HomePage/modules/ListCinemaShowtimeReducer";
@@ -17,7 +17,7 @@ import detailInfoReducer from "../../containers/AdminTemplate/DashBoardPage/modu
 const rootReducer = combineReducers({
   listMovieReducer,
   NewReducers,
-  btgamebaucuareducer,
+ 
   DetailMovieReducer,
   listCinemaReducer,
   listCinemaShowtimeReducer,
